50533a41a3fe561ff6573fe2ecd1f293913d69cf
ishai asor <ishaiasor.consist@gmail.com>
2026-03-01 09:36:05 +0200

    fix env

--- backend/config/env.config.js
+++ backend/config/env.config.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 module.exports = {
   port: process.env.PORT || 3000,
   mqtt: {
-    brokerUrl: process.env.MQTT_BROKER_URL || 'mqtt://test.mosquitto.org',
+    brokerUrl: process.env.MQTT_BROKER_URL,
     username: process.env.MQTT_USERNAME,
     password: process.env.MQTT_PASSWORD
   },
   db: {
     // We will use these later when swapping the mock for the real pg library
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    database: process.env.DB_NAME || 'smarthome_db'
+    host: process.env.DB_HOST ,
+    user: process.env.DB_USER ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME 
   }
 };
