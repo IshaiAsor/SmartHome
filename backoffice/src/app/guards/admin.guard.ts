cf56480aa63e33c40ffce4b82abcd69b4595234b
Ishai Asor <ishaiasor@gmail.com>
2026-06-14 17:17:59 +0300

    fix guards

--- backoffice/src/app/guards/admin.guard.ts
+++ backoffice/src/app/guards/admin.guard.ts
@@ -2,7 +2,7 @@ import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 
-export const adminGuard: CanActivateFn = (_route, _state) => {
+export const adminGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
